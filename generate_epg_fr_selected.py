73de14eadcb510d4ea0d88bd04c819669ddf8aa2
Ayoub Boukous <37737944+ayoubboukous27@users.noreply.github.com>
2026-03-21 18:21:20 +0100

    Update generate_epg_fr_selected.py

--- generate_epg_fr_selected.py
+++ generate_epg_fr_selected.py
@@ -12,6 +12,7 @@ root_src = ET.fromstring(r.content)
 # القنوات المطلوبة والشعارات
 logos = {
     "TF1.fr": "https://raw.githubusercontent.com/ayoubboukous27/Event/refs/heads/main/TF1_logo_1990.svg.png",
+    "TF14k.fr": "https://raw.githubusercontent.com/ayoubboukous27/Event/refs/heads/main/Tf14k-logo.png",
     "France2.fr": "https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/france/france-2-fr.png",
     "France3.fr": "https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/france/france-3-fr.png",
     "France4.fr": "https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/france/france-4-fr.png",
