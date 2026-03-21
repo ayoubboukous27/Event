56a185c50530d726654ad491c8eb809b36120f9e
Ayoub Boukous <37737944+ayoubboukous27@users.noreply.github.com>
2026-03-21 17:45:31 +0100

    Update generate_epg_remote.py

--- generate_epg_remote.py
+++ generate_epg_remote.py
@@ -13,10 +13,10 @@ root_src = ET.fromstring(r.content)
 # إنشاء TV جديد
 tv = ET.Element("tv")
 
-# إضافة القناة مع شعارك الجديد
+# إضافة القناة مع الشعار الجديد
 channel = ET.SubElement(tv, "channel", id="canal_plus_evenements_4k")
 ET.SubElement(channel, "display-name").text = "Canal + Événements 4K"
-ET.SubElement(channel, "icon", src="https://raw.githubusercontent.com/ayoubboukous27/Event/refs/heads/main/Picsart_26-03-21_17-12-28-760.png")
+ET.SubElement(channel, "icon", src="https://raw.githubusercontent.com/ayoubboukous27/Event/refs/heads/main/Picsart_26-03-21_17-41-51-504.png")
 
 # نسخ جميع برامج القناة الأصلية مع تغيير الاسم
 for prog in root_src.findall('programme[@channel="EvenementsSports4KUHD.fr"]'):
