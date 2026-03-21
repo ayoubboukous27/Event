9370cb5bb2553820d358e5b55fc9179a233377cc
Ayoub Boukous <37737944+ayoubboukous27@users.noreply.github.com>
2026-03-21 18:32:07 +0100

    Update generate_epg_fr_selected.py

--- generate_epg_fr_selected.py
+++ generate_epg_fr_selected.py
@@ -17,6 +17,7 @@ logos = {
     "France3.fr": "https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/france/france-3-fr.png",
     "France4.fr": "https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/france/france-4-fr.png",
     "France5.fr": "https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/france/france-5-fr.png",
+    "CanalPlusEvenements.fr": "https://raw.githubusercontent.com/ayoubboukous27/Event/refs/heads/main/Picsart_26-03-21_17-41-51-504.png",
 }
 
 # إنشاء ملف XMLTV جديد
